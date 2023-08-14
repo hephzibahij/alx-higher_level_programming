503e695733bba92a107d637b43e966774c34012d
Hephzibah <hephzibahijeje89@gmail.com>
2023-08-14 14:10:44 -0700

    8-multiple_returns.py

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if len(sentence) == 0:
+        first_char = None
+    else:
+        first_char = sentence[0]
+
+    result_tuple = (len(sentence), first_char)
+    return result_tuple
