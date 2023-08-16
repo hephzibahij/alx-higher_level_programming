83db1f62d593d65c1c4d514e9d768de21b931863
Hephzibah <hephzibahijeje89@gmail.com>
2023-08-16 14:58:01 -0700

    0-square_matrix_simple.py

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    if matrix == []:
+        return []
+
+    new_matrix = []
+
+    for row in matrix:
+        squared_row = [x ** 2 for x in row]
+        new_matrix.append(squared_row)
+
+    return new_matrix
