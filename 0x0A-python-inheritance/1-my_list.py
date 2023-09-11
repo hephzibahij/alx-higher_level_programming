82f988b075a2d4c848d7623c14c5be066c486f05
Hephzibah <hephzibahijeje89@gmail.com>
2023-09-11 11:45:46 -0700

    task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
-"""
-contains the MyList class
-"""
+"""Module that inherits a list and prints the element"""
 
 
 class MyList(list):
-    """a subclass of list"""
-    def __init__(self):
-        """initializes the object"""
-        super().__init__()
-
+    """My list class that inherits list and prints its attribute"""
     def print_sorted(self):
-        """prints the sorted list"""
+        """Method that prints sorted list"""
         print(sorted(self))
