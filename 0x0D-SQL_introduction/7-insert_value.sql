4b0d98f786b466d324b914aeb100aff74c3f4f4e
Hephzibah <hephzibahijeje89@gmail.com>
2023-10-18 05:55:09 -0700

    7-insert_value.sql

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,3 @@
--- Insert a new row in the table
-INSERT into 'first_table' (id, name) VALUES(89, 'Best School');
+-- Inserts a new row in a table
+
+INSERT INTO `first_table` (id, name) VALUES(89, 'Best School');
