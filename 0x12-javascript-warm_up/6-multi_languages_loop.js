374a1acae1b92bad85ca90785039cb7955c0c959
Zibah <hephzibahijeje89@gmail.com>
2023-11-14 05:34:16 -0800

    6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // Array of strings
-const languages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 // Loop to print each line
 for (let i = 0; i < languages.length; i++) {
