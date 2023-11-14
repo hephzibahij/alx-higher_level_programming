3bb7004c17b30d621a94643f302080fde593f528
Zibah <hephzibahijeje89@gmail.com>
2023-11-14 03:22:35 -0800

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,9 +2,9 @@
 // Checking the number of arguments and printing messages accordingly
 const length = process.argv.length - 2;
 if (length === 0) {
-    console.log('No argument');
-} else if(length === 1) {
-    console.log('Argument found');
+  console.log('No argument');
+} else if (length === 1) {
+  console.log('Argument found');
 } else {
-    console.log('Arguments found');
+  console.log('Arguments found');
 }
