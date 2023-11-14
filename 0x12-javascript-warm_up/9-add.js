721533339f3d7f6021f06b6399beb71de7cafe2d
Zibah <hephzibahijeje89@gmail.com>
2023-11-14 11:07:31 -0800

    9-add.js

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// Define the function add with two parameters
+const add = (a, b) => {
+  // Calculate the sum of the two integers
+  const sum = a + b;
+
+  // Print the result using console.log
+  console.log('The sum is:', sum);
+};
