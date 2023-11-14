2d80d73fe4e14cee8559b05198f567169416fa09
Zibah <hephzibahijeje89@gmail.com>
2023-11-14 09:50:38 -0800

    7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,16 +1,13 @@
 #!/usr/bin/node
 // Accessing the first argument from the command line
-const arg = process.argv[2];
-
 // Attempting to convert the argument to an integer
-const numOccurrences = parseInt(arg);
-
 // Checking if the conversion was successful and printing the result
-if (!isNaN(numOccurrences)) {
-    console.log('Missing number of occurrences');
+const numOccurrences = Number(process.argv[2]);
+
+if (!isNaN(numOccurrences) && Number.isInteger(numOccurrences)) {
+  for (let i = 0; i < numOccurrences; i++) {
+    console.log('C is fun');
+  }
 } else {
-    // Loop to print "C is fun" x times
-    for (let i = 0; i < numOccurrences; i++) {
-        console.log('C is fun');
-    }
+  console.log('Missing number of occurrences');
 }
