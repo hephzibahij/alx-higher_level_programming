089e818caeee468ff83e276ad010edadf8d8b85e
Zibah <hephzibahijeje89@gmail.com>
2023-11-14 05:03:41 -0800

    4-concat.js

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// Accessing the first and second arguments from the command line
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+
+// Printing the two arguments in the specified format
+console.log(`${arg1} is ${arg2}`);
