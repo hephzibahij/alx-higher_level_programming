26e5db35cae783a3d63bdc5581b69ed890f76084
Zibah <hephzibahijeje89@gmail.com>
2023-11-14 10:10:04 -0800

    8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,20 +1,13 @@
 #!/usr/bin/node
 // Accessing the first argument from the command line
-const arg = process.argv[2];
-
+const size = Number(process.argv[2]);
 // Attempting to convert the argument to an integer
-const size = parseInt(arg);
-
-// Checking if the conversion was successful and printing the result
-if (isNaN(size)) {
-    console.log('Missing size');
+// Checking if the conversion was successful and printing the resul
+// Loop to print a square of size x
+if (!isNaN(size) && Number.isInteger(size)) {
+  for (let i = 0; i < size; i++) {
+    console.log('X'.repeat(size));
+  }
 } else {
-    // Loop to print a square of size x
-    for (let i = 0; i < size; i++) {
-        let row = '';
-        for (let j = 0; j < size; j++) {
-            row += 'X';
-        }
-        console.log(row);
-    }
+  console.log('Missing size');
 }
